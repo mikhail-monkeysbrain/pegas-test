599cd43f0b16525e63b1d764723a0fdba3d20b5f
monkeysbrain <monkeysbrain@yandex.ru>
2019-12-13 05:42:41 +0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+
+    <!-- header -->
+
+    <header class="header">
+      <a href="./">
+        <img class="logo" src="./assets/img/logo-top.svg" alt="Nordwind Airlines">
+      </a>
+      <div class="header-nav">
+        <a class="header-nav__item" href="./">
+          <img class="header-nav__icon" src="./assets/img/rules.svg">
+          Правила и сроки рассмотрения обращений
+        </a>
+        <a class="header-nav__item" href="./">
+          <img class="header-nav__icon" src="./assets/img/back.svg">
+          Назад на nordwindairlines.ru
+        </a>
+      </div>
+    </header>
+
+    <section>
+      <div class="container">
+        <div id="app">
+          <h1>Центр поддержки пассажиров Nordwind Airlines</h1>
+        </div>
+      </div>
+    </section>
+
+    <footer class="footer">
+
+        <div class="container">
+          <div class="footer__top">
+            <div class="footer__phones">
+              <div class="phone">
+                <span>Для Москвы</span>
+                <a class="tel" href="tel:+74957305080">+7 495 730 50 80</a>
+              </div>
+              <div class="phone">
+                <span>Звонок по России бесплатный</span>
+                <a class="tel" href="tel:88002224844">8 800 222 48 44</a>
+              </div>
+            </div>
+
+            <ul class="social">
+              <li class="social__item">
+                <a href="./">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="social__item">
+                <a href="./">
+                  <i class="fab fa-vk"></i>
+                </a>
+              </li>
+              <li class="social__item">
+                <a href="./">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li class="social__item">
+                <a href="./">
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="footer__bottom">
+          <div class="container">
+            <p class="copy">© 2019 Северный ветер. Все пpава защищены.</p>
+            <img class="iata-logo" src="./assets/img/iata.svg" alt="iata">
+            <p class="rules">Пользуясь сайтом support.nordwindairlines.ru, Вы автоматически принимаете <a href="./">правила</a> передачи и обработки персональных данных.</p>
+          </div>
+        </div>
+
+    </footer>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './assets/stylus/main.styl'
+
 Vue.config.productionTip = false
 
 new Vue({
